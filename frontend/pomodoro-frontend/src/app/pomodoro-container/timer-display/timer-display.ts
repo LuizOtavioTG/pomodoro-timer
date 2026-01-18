757b07eee5f8c9207b37e25cdc5cf653d40c6de0
LuizOtavioTG <bc.luizo@gmail.com>
2026-01-18 16:20:00 -0300

    feat: add timer countdown logic and render on view

--- frontend/pomodoro-frontend/src/app/pomodoro-container/timer-display/timer-display.ts
+++ frontend/pomodoro-frontend/src/app/pomodoro-container/timer-display/timer-display.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-timer-display',
@@ -6,6 +7,26 @@ import { Component } from '@angular/core';
   templateUrl: './timer-display.html',
   styleUrl: './timer-display.scss',
 })
-export class TimerDisplay {
+export class TimerDisplay implements OnInit, OnDestroy {
 
+  seconds = 0;
+  minutes = 0;
+  formatedTimer = "00:00";
+
+  private sub?: Subscription;
+
+  ngOnInit(): void {
+    this.sub = interval(1000).subscribe(() => {
+      this.seconds++;
+      this.minutes = Math.floor(this.seconds / 60);
+
+      const mins = String(this.minutes).padStart(2, '0');
+      const secs = String(this.seconds % 60).padStart(2, '0');
+
+      this.formatedTimer = `${mins}:${secs}`;
+    });
+  }
+  ngOnDestroy(): void {
+    this.sub?.unsubscribe();
+  }
 }
